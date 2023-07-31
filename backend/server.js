96b11e735571a79c2244d109052743a31efd005e
annhuny <annhuny@gmail.com>
2023-07-31 22:52:50 +0100

    auth user skeleton

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config()
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 connectDB()
 
@@ -16,9 +17,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
+
 app.listen(port, () => {
   console.log(`server running on ${port}`)
 })
