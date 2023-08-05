d1a6535f3a30149d07bdef09b8f0d45315237735
annhuny <annhuny@gmail.com>
2023-08-05 23:01:48 +0100

    Frontend - Checkout screent

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { saveShippingAddress } from '../slices/cartSlice'
@@ -25,6 +26,9 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+
+      <CheckoutSteps step1 step2/>
+
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
