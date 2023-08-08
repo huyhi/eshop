6c79e602fbdee5515047a9fbd31c49016688c10a
annhuny <annhuny@gmail.com>
2023-08-08 23:27:59 +0100

    part of order api

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,66 @@
+import Order from '../models/orderModel.js'
+import asyncHandler from '../middleware/asyncHandler.js'
+
+
+export const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No order items')
+  }
+
+  const order = new Order({
+    orderItems: orderItems.map((x) => ({
+      ...x, 
+      product: x._id,
+      _id, undefined
+    })),
+    user: req.user._id,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice
+  })
+
+  const createOrder = await order.save()
+  res.status(201).json(createOrder)
+})
+
+export const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({user: req.user._id})
+  res.status(200).json(orders)
+})
+
+export const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (!order) {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+
+  res.status(200).json(order)
+})
+
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  res.json('updateOrderToPaid')
+})
+
+export const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.json('updateOrderToDelivered')
+})
+
+export const getOrders = asyncHandler(async (req, res) => {
+  res.json('getOrders')
+})
---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,30 @@
+import express from "express"
+const router = express.Router()
+import { 
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  getOrders,
+  updateOrderToDelivered,
+  updateOrderToPaid
+} from '../controllers/orderController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+router.route('/')
+  .get(protect, admin, getOrders)
+  .post(protect, addOrderItems)
+
+router.route('/mine')
+  .get(protect, getMyOrders)
+
+router.route('/:id')
+  .get(protect, admin, getOrderById)
+
+router.route('/:id/pay')
+  .get(protect, updateOrderToPaid)
+
+router.route('/:id/deliver')
+  .get(protect, admin, updateOrderToDelivered)
+
+
+export default router
